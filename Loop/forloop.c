b3c4245f37226bb495387e6f0a71051c7c7f5e8e
Sharmin710 <sharminaktermim58@gmail.com>
2023-12-02 13:26:28 +0600

    I am pushing my first folder to github

---
+++ Loop/forloop.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main(){
+
+int i;
+for (i = 1; i<=100; i++){
+
+
+    printf ("Miss u Nuri,Faru,Ritu\n");
+}
+
+return 0;
+}
---
+++ Loop/forloop1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+
+ int i;
+
+ for(i=1;i<=5;i++){
+
+    printf("%d\n",i);
+ }
+
+    printf("Out of loop");
+
+
+
+
+
+    return 0;
+}
